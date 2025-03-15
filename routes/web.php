bdeb1e3c0cd04739ef2c7debdc399239d62e14d8
khaledgamalramadan <bebo50016@gmail.com>
2025-03-15 16:31:40 +0200

    install laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,13 @@
 <?php
 
-use App\Http\Controllers\ThemeController;
-use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\View;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-
+|
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
@@ -20,19 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Theme Routes
-Route::controller(ThemeController::class)->group(function () {
-    Route::get('/index', 'index');
-    Route::get('/about', 'about');
-    Route::get('/contact', 'contact');
-    Route::get('/services', 'services');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-
-Route::controller(AdminController::class)->group(function () {
-    Route::get('/admin', 'user');
-    Route::get('/admin/services', 'services');
-    Route::get('/admin/apartments', 'apartments');
-
-});
-
+require __DIR__.'/auth.php';
